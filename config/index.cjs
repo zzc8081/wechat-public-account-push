fb1068625ce297ac365dbba15d8e4f74fc03b6fa
zzc8081 <39311643+zzc8081@users.noreply.github.com>
2022-11-29 16:31:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,12 +7,13 @@
  */
 const USER_CONFIG = {
 
+  USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
-
+  
   PROVINCE: '广东',
   CITY: '惠州',
 
@@ -21,9 +22,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU18287TDCYNQEE6ZglItrzbJRSQEKHKeu2nYwDp',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
